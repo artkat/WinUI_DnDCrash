ed96c78b7ec28b5bdad55491ea0abbd1eb85b1e4
Arthur Kater <plazalogic@biochek.com>
2022-04-21 15:46:29 +0800

    Initial
    Source code added

---
+++ DnDCrash/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace AppPdf
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+        
+        int dropCounter;
+     
+        private void Source_DragStarting(UIElement sender, DragStartingEventArgs args)
+        {
+            args.Data.RequestedOperation  = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Move;
+            
+        }
+
+        private async void Target_Drop(object sender, DragEventArgs e)
+        {
+            Target.Text = $"Dropped {++dropCounter}";
+        }
+
+        private void Target_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Move;
+        }
+    }
+}
